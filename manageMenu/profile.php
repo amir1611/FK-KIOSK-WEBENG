d768273e2a7348d5762019c7dcb942f9dc70b834
Amir <amirjr1611@gmail.com>
2024-01-09 23:28:40 +0800

    test (#3)
    * Manage menu (#2)
    
    * add index.php
    
    * added manage menu files
    
    * added ui elements
    
    * added display menu page
    
    * updated to php files
    
    * added insert data
    
    * added module 2 sql file
    
    ---------
    
    Co-authored-by: haziq-danial <haziqdanial031@gmail.com>
    
    * update presentation
    
    ---------
    
    Co-authored-by: haziq-danial <haziqdanial031@gmail.com>

--- manageMenu/profile.php
+++ manageMenu/profile.php
@@ -1,11 +1,54 @@
+<?PHP
+session_start();
 
+include("../config/database.php");
+if (!verifyUser($con)) {
+	header("Location: index.php");
+	return false;
+}
+?>
+<?PHP
+$id_user	= $_SESSION["id_user"];
+$act 		= (isset($_REQUEST['act'])) ? trim($_REQUEST['act']) : '';
+
+$name 		= (isset($_POST['name'])) ? trim($_POST['name']) : '';
+$email		= (isset($_POST['email'])) ? trim($_POST['email']) : '';
+$phone		= (isset($_POST['phone'])) ? trim($_POST['phone']) : '';
+$password	= (isset($_POST['password'])) ? trim($_POST['password']) : '';
+
+$success = "";
+
+if ($act == "edit") {
+	$SQL_update = " 
+	UPDATE
+		`user`
+	SET
+		`name` = '$name',
+		`email` = '$email',
+		`phone` = '$phone',
+		`password` = '$password'
+	WHERE
+		`id_user`='$id_user' 
+		";
+
+	$result = mysqli_query($con, $SQL_update);
+
+	$success = "Successfully Update";
+	//print "<script>self.location='a-profile.php';</script>";
+}
+
+$SQL_list = "SELECT * FROM `user` WHERE `id_user` = '$id_user' ";
+$result = mysqli_query($con, $SQL_list);
+$data	= mysqli_fetch_array($result);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Kiosk</title>
-    <link rel="stylesheet" href="../../w3.css">
+	<title>FKKIOSK</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../../w3.css">
 	<link href='https://fonts.googleapis.com/css?family=RobotoDraft' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -61,7 +104,14 @@
 
 <body class="w3-biru">
 
+	<!--- Toast Notification -->
+	<?PHP
+	if ($success) {
+		Notify("success", $success, "profile.php");
+	}
+	?>
 
+	
 	<!-- Side Navigation -->
 	<nav class="w3-sidebar w3-bar-block w3-collapse  w3-card" style="z-index:3;width:250px; background:#e7c0ff;" id="mySidebar">
 		<a href="dashboard.php" class="w3-bar-item w3-large" style="border-bottom: 2px solid #877272f0;"><img src="../../images/logo.png" class="w3-padding" style="width:216px;"></a>
@@ -94,28 +144,23 @@
 
 
 
-		<!-- navbar -->
-        <div class="w3-white w3-bar w3-card ">
+		<div class="w3-white w3-bar w3-card ">
 
 
 			<i class="fa fa-bars w3-buttonx w3-white w3-hide-large w3-xlarge w3-margin-left w3-margin-top" onclick="w3_open()"></i>
 
 
 			<div class="w3-large w3-buttonx w3-bar-item w3-right w3-white w3-dropdown-hover">
-				<button class="w3-button"><i class="fa fa-fw fa-user-circle"></i> Vendor <i class="fa fa-fw fa-chevron-down w3-small"></i></button>
-				<div class="w3-dropdown-content w3-bar-block w3-card-4">
-					<a href="../../manageAccount/profile.php" class="w3-bar-item w3-button"><i class="fa fa-fw fa-user-cog "></i> Profile</a>
-					<a href="../../config/userlogout.php" class="w3-bar-item w3-button"><i class="fa fa-fw fa-sign-out-alt "></i> Signout</a>
+				<button class="w3-button"><i class="fa fa-fw fa-user-circle"></i> User <i class="fa fa-fw fa-chevron-down w3-small"></i></button>
+				<div class="w3-dropdown-content w3-bar-block w3-card-4" style="min-width: 54px;">
+					<a href="profile.php" class="w3-bar-item w3-button"><i class="fa fa-fw fa-user-cog "></i> Profile</a>
+					<a href="../../config/userlogout.php" class="w3-bar-item w3-button"><i class="fa fa-fw fa-sign-out-alt "></i> Logout</a>
 				</div>
 			</div>
 
 		</div>
 
-
-
 		<div class="w3-padding-32"></div>
-
-
 
 		<div class="w3-container">
 
@@ -127,41 +172,33 @@
 					<form action="" method="post">
 						<div class="w3-padding">
 							<div style="text-align: center;">
-								<b class="w3-large">Kiosk Details</b>
+								<b class="w3-large">Profile</b>
 							</div>
 
 							<hr>
 
 							<div class="w3-section">
-								<label style="font-weight: bold; color: black;">Kiosk Name <span style="color: red;">*</span></label>
+								<label style="font-weight: bold; color: black;">Full Name <span style="color: red;">*</span></label>
 
-								<input class="w3-input w3-border w3-round" type="text" name="name" value="" required>
+								<input class="w3-input w3-border w3-round" type="text" name="name" value="<?PHP echo $data["name"]; ?>" required>
 							</div>
 
 							<div class="w3-section">
-								<label style="font-weight: bold; color: black;">Enter Operating Hours <span style="color: red;">*</span></label>
+								<label style="font-weight: bold; color: black;">Contact <span style="color: red;">*</span></label>
 
-								<input class="w3-input w3-border w3-round" type="text" name="phone" value="" required>
+								<input class="w3-input w3-border w3-round" type="text" name="phone" value="<?PHP echo $data["phone"]; ?>" required>
 							</div>
 
 							<div class="w3-section">
-								<label style="font-weight: bold; color: black;">Description <span style="color: red;">*</span></label>
-								<input class="w3-input w3-border w3-round" type="text" name="phone" value="" required>
+								<label style="font-weight: bold; color: black;">Email <span style="color: red;">*</span></label>
+
+								<input class="w3-input w3-border w3-round" type="email" name="email" value="<?PHP echo $data["email"]; ?>" required>
 							</div>
 
 							<div class="w3-section">
-								<label style="font-weight: bold; color: black;">Upload Image <span style="color: red;">*</span></label>
+								<label style="font-weight: bold; color: black;">Password <span style="color: red;">*</span></label>
 
-								<input type="file" name="image">
-							</div>
-
-							<div class="w3-section">
-								<label style="font-weight: bold; color: black;">Kiosk Status <span style="color: red;">*</span></label>
-								<span class="w3-badge w3-large w3-green">Open</span>
-								<select class="w3-input w3-border w3-round" name="role" required>
-									<option value="open">Open</option>
-									<option value="close">Close</option>
-								</select>
+								<input class="w3-input w3-border w3-round" type="password" name="password" value="<?PHP echo $data["password"]; ?>" required>
 							</div>
 
 							<hr class="w3-clear">
@@ -187,14 +224,13 @@
 		<!-- container end -->
 
 
-
-
-		<footer class="w3-container w3-padding-1 w3-center" style="background: white;margin-top: 625px;">
+		<footer class="w3-container w3-padding-1 w3-center" style="background: white;margin-top: 209px;">
 			<p>&copy; 2023 FK KIOSK. All rights reserved.</p>
 		</footer>
 
 
 	</div>
+
 
 	<script>
 		var openInbox = document.getElementById("myBtn");
@@ -222,5 +258,7 @@
 			}
 		}
 	</script>
+
 </body>
+
 </html>
